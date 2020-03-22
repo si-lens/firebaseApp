4cced5c0b83cde5feede9d3d14d7b95351ca2892
Szymon Soczewka <42994488+Sajmonoo1998@users.noreply.github.com>
2020-03-22 14:53:40 +0100

    Update alert-service.spec.ts

--- src/app/shared/alert-service.spec.ts
+++ src/app/shared/alert-service.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-
-import { AlertServiceService } from './alert-service.service';
+import {AlertService} from './alert-service.service';
 
 describe('AlertServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: AlertServiceService = TestBed.get(AlertServiceService);
+    const service: AlertService = TestBed.get(AlertService);
     expect(service).toBeTruthy();
   });
 });
